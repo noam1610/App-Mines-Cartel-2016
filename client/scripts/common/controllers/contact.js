66345f6b617d326f87cde090ac7e231c31f45d5a
noam1610 <nbenlolo@yoobic.com>
2016-04-20 10:26:11 +0200

    WIP

--- client/scripts/common/controllers/contact.js
+++ client/scripts/common/controllers/contact.js
@@ -15,9 +15,11 @@ module.exports = function(app) {
         vm.height = $window.innerHeight;
 
         vm.listDesContacts = [{
-            'Name': 'Mines De Paris',
-            'Addresse': 'images/app/logo.png',
-            'description': 'Bla kjdsf ldkf skdjf'
+            'Name': 'Hotline',
+            'numero1': '06 48 07 98 26',
+            'numero2': '06 40 45 55 18',
+            'horaires': '7h30 à 21h30',
+            'mail': 'contact@cartel2016.com'
         }];
 
         vm.myGoBack = function() {
